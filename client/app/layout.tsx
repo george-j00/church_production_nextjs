b042bd077505d30769fe1f47223d6a4f84a0dded
GeorgeJose <subingeorge027@gmail.com>
2024-03-31 22:07:27 +0530

    fix layout

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,9 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
+
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
+const inter = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
   title: "St.Antony's Jacobite Syrian Cathedral",
