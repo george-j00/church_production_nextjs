623aadde3661d6347178c786b41451ae7d0dea3e
GeorgeJose <subingeorge027@gmail.com>
2024-04-07 23:48:31 +0530

    fix issue

--- client/app/(admin)/admin/dashboard/page.tsx
+++ client/app/(admin)/admin/dashboard/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const Dashboard = () => {
   return (
-    <>
+    <> 
       <h1 className='text-center mt-2 '>Welcome to the admin page! Here you can manage content, events, and settings for our church website</h1>
     </>
   );
