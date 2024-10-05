50fdd41efa11a6740c93492fb7c04607407c44a6
GeorgeJose <george@kumohq.co>
2024-10-05 16:05:38 +0530

    fix issues

--- client/app/(root)/page.tsx
+++ client/app/(root)/page.tsx
@@ -1,17 +1,19 @@
-import Contact from "@/components/pages/contact/Contact";
-import EventPage from "@/components/pages/event/Events";
-import Header from "@/components/pages/landing/Header";
-import Hero from "@/components/pages/hero/Hero";
+import Contact from "@/components/pages/contact/Contact"
+import EventPage from "@/components/pages/event/Events"
+import Header from "@/components/pages/landing/Header"
+import Hero from "@/components/pages/hero/Hero"
+import StAntonyPage from "@/components/pages/st-antony/StAntonyPage"
 
 export default function Home() {
   return (
     <>
       <section className="flex flex-col gap-8">
         <Header />
+        <StAntonyPage />
         <EventPage />
         <Hero />
         <Contact />
       </section>
     </>
-  );
+  )
 }
--- client/app/(root)/relics/page.tsx
+++ client/app/(root)/relics/page.tsx
@@ -1,7 +1,7 @@
-"use client";
+"use client"
 
-import Image from "next/image";
-import React, { useState } from "react";
+import Image from "next/image"
+import React, { useState } from "react"
 
 const Relics = () => {
   const relics = [
@@ -10,9 +10,9 @@ const Relics = () => {
       name: "St. Mary",
       title: "Girdle of St. Mary",
       detail:
-        "The relics of St. Mary are not only historical artifacts but also spiritual treasures for many Christians. They are believed to convey Mary's presence and intercessory power, providing comfort, inspiration, and miracles to the faithful. These relics are often housed in ornate reliquaries and displayed during important liturgical feasts, particularly on Nativity of the Blessed Virgin Mary (September 8).",
+        "The relics of St. Mary are not only historical artifacts but also spiritual treasures for many Christians. They are believed to convey Mary's presence and intercessory power, providing comfort, inspiration, and miracles to the faithful. These relics are often housed in ornate reliquaries and displayed during important liturgical feasts, particularly on the Nativity of the Blessed Virgin Mary (September 8).",
       description:
-        'Also known as the "Holy Belt," this is a belt or girdle said to have belonged to Mary',
+        'Also known as the "Holy Belt," this is a belt or girdle said to have belonged to Mary.',
       significance:
         "Symbolizes Mary's maternal protection and her role as a mediator between humanity and God.",
     },
@@ -21,47 +21,50 @@ const Relics = () => {
       name: "St. Ignatius Elias III",
       title: "Iconographic Relics",
       detail:
-        "The relics of St. Mary are not only historical artifacts but also spiritual treasures for many Christians. They are believed to convey Mary's presence and intercessory power, providing comfort, inspiration, and miracles to the faithful. These relics are often housed in ornate reliquaries and displayed during important liturgical feasts, particularly on Nativity of the Blessed Virgin Mary (September 8).",
+        "St. Elias III, also known as Mor Ignatius Elias III, was a significant patriarch in the Syriac Orthodox Church, serving as the 119th Patriarch of Antioch. Born in 1867 in Mardin, Turkey, and passing away in 1932 in India, St. Elias III is remembered for his piety, leadership, and efforts to maintain the unity of the Syriac Orthodox Church during tumultuous times. His relics are revered by the faithful, especially in the places most connected to his life and work.",
       description:
-        'Also known as the "Holy Belt," this is a belt or girdle said to have belonged to Mary',
+        "Icons and images depicting St. Elias III, often containing small relics such as pieces of his clothing or hair.",
       significance:
-        "Symbolizes Mary's maternal protection and her role as a mediator between humanity and God.",
+        "These relics serve as visual and physical reminders of the patriarch’s sanctity and are used in veneration practices.",
     },
     {
       image: "/assets/relics/Relic3.jpg",
       name: "St. Gregorios of Parumala",
       title: "Iconographic Relics",
       detail:
-        "The relics of St. Mary are not only historical artifacts but also spiritual treasures for many Christians. They are believed to convey Mary's presence and intercessory power, providing comfort, inspiration, and miracles to the faithful. These relics are often housed in ornate reliquaries and displayed during important liturgical feasts, particularly on Nativity of the Blessed Virgin Mary (September 8).",
+        "Saint Gregorios of Parumala, also known as Parumala Thirumeni, is a revered saint in the Malankara Orthodox Syrian Church. Born as Geevarghese in 1848 and passing away in 1902, he was canonized in 1947. Known for his deep spirituality, ascetic life, and healing powers, his relics are considered holy and are venerated by the faithful.",
       description:
-        'Also known as the "Holy Belt," this is a belt or girdle said to have belonged to Mary',
+        "Icons and images depicting St. Gregorios, sometimes containing small relics such as pieces of his clothing.",
       significance:
-        "Symbolizes Mary's maternal protection and her role as a mediator between humanity and God.",
+        "These relics serve as visual and physical reminders of the saint’s sanctity and are used in veneration practices.",
     },
-  ];
+  ]
 
   // State to manage the visibility of the modal
-  const [modalOpen, setModalOpen] = useState(false);
-  // State to store the URL of the selected image
-  const [selectedImage, setSelectedImage] = useState({
+  const [modalOpen, setModalOpen] = useState(false)
+  // State to store the URL and description of the selected image
+  const [selectedImage, setSelectedImage] = useState<{
+    description: string
+    image: string
+  }>({
     description: "",
     image: "",
-  });
+  })
 
   // Function to handle click on an image and open the modal
-  const handleImageClick = (image : any) => {
-    setSelectedImage(image);
-    setModalOpen(true);
-  };
+  const handleImageClick = (image: { description: string; image: string }) => {
+    setSelectedImage(image)
+    setModalOpen(true)
+  }
 
   // Function to close the modal
   const closeModal = () => {
+    setModalOpen(false)
     setSelectedImage({
       description: "",
       image: "",
-    });
-    setModalOpen(false);
-  };
+    })
+  }
 
   return (
     <div className="container mx-auto px-4 py-8">
@@ -74,56 +77,41 @@ const Relics = () => {
         Latin word reliquiae, which means remains.
       </p>
 
-      <div className="grid md:grid-cols-3 gap-6 grid-cols-1">
-        {relics?.map((relic, index) => (
+      <div className="grid md:grid-cols-3 gap-8 grid-cols-1">
+        {relics.map((relic, index) => (
           <div
             key={index}
-            className="flex flex-col items-center justify-center"
+            className="flex flex-col bg-white rounded-lg shadow-md overflow-hidden h-full"
           >
-            <div className="overflow-hidden rounded-lg shadow-md w-72 h-72">
+            <div className="relative w-full h-80">
               <Image
-                width={300} // Set the width to 300
-                height={300} // Set the height to 300
-                src={relic?.image}
-                alt={`Church Image ${index + 1}`}
-                className="w-full h-full object-cover cursor-pointer"
+                src={relic.image}
+                alt={`${relic.name} Image`}
+                layout="fill"
+                objectFit="contain" // Ensures the entire image is visible without cropping
+                className="cursor-pointer transition-transform transform hover:scale-105 duration-300"
                 onClick={() => handleImageClick(relic)}
               />
             </div>
 
-            <div className="md:pl-4 mt-4 text-justify">
-              <h3 className="text-lg font-bold mb-2">{relic?.name}</h3>
-              <h4 className="text-indigo-500 font-bold mb-2">{relic?.title}</h4>
-              <p className="text-gray-600 mb-2">{relic?.detail}</p>
-              <p className="text-gray-600 mb-2">{relic?.description}</p>
-              <p className="text-gray-600">{relic?.significance}</p>
+            <div className="flex flex-col flex-grow p-4">
+              <h3 className="text-lg font-bold mb-2 text-center">
+                {relic.name}
+              </h3>
+              <h4 className="text-indigo-500 font-bold mb-2 text-center">
+                {relic.title}
+              </h4>
+              <p className="text-gray-600 mb-2 text-justify">{relic.detail}</p>
+              <p className="text-gray-600 mb-2 text-justify">
+                {relic.description}
+              </p>
+              <p className="text-gray-600 text-justify">{relic.significance}</p>
             </div>
           </div>
         ))}
       </div>
-
-      {/* Modal for displaying enlarged image (unchanged) */}
-      {modalOpen && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
-          <div className="relative max-w-full max-h-full">
-            <button
-              className="absolute top-0 right-0 m-4 text-gray-100 text-2xl cursor-pointer"
-              onClick={closeModal}
-            >
-              close &times;
-            </button>
-            <Image
-              width={300} // Set the width to 300
-              height={300} // Set the height to 300
-              src={selectedImage?.image}
-              alt="Enlarged Image"
-              className="max-w-full max-h-full"
-            />
-          </div>
-        </div>
-      )}
     </div>
-  );
-};
+  )
+}
 
-export default Relics;
+export default Relics
