d1fd1d527f2ca771e5fa58def4a40c8bf43c6f26
george-j00 <dev.georgejose@gmail.com>
2025-04-15 01:20:06 +0530

    remove client

--- client/app/api/logout/route.ts
+++
@@ -1,9 +0,0 @@
-// app/api/logout/route.js
-import { cookies } from 'next/headers';
-import { NextResponse } from 'next/server';
-
-export async function POST() {
-    const response = NextResponse.json({ message: 'Logged out successfully' });
-    cookies().delete('Jwt_login_token')
-    return response;
-}
