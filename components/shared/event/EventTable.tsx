05a10a8100dd2aefd22d7953c976a3b2e201c4a4
george-j00 <dev.georgejose@gmail.com>
2025-04-15 00:23:25 +0530

    update event table

--- components/shared/event/EventTable.tsx
+++ components/shared/event/EventTable.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 
 import {
   Table,
@@ -7,54 +7,75 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table";
-import { fetchAllEvents } from "@/lib/actions/admin.actions";
-import { EventParams } from "@/types";
-import useSWR from 'swr';
+} from "@/components/ui/table"
+import { fetchAllEvents } from "@/lib/actions/admin.actions"
+import { EventParams } from "@/types"
+import useSWR from "swr"
 
 const fetcher = async () => {
-  const data = await fetchAllEvents('Upcoming');
+  const data = await fetchAllEvents("Upcoming")
+  console.log("upcoming events", data)
+
   return data?.map((event: { eventDate: string | number | Date }) => {
     // Convert eventDate to Date object
-    const formattedDate = new Date(event.eventDate).toLocaleDateString();
+    const formattedDate = new Date(event.eventDate).toLocaleDateString()
     // Replace the original eventDate with the formattedDate
-    return { ...event, eventDate: formattedDate };
-  });
-};
+    return { ...event, eventDate: formattedDate }
+  })
+}
 
 export function EventTable() {
-  const { data: events, error } = useSWR('/api/events/upcoming', fetcher);
+  const { data: events, error } = useSWR("/api/events/upcoming", fetcher)
 
-  if (error) return <p className="text-center text-xl mt-20 text-black-500">Failed to load events</p>;
-  if (!events) return <p className="text-center text-xl mt-20 text-black-500">...loading</p>;
-  if (events.length <= 0 ) return <p className="text-center text-xl mt-20 text-black-500">No events</p>;
+  if (error)
+    return (
+      <p className="text-center text-xl mt-20 text-black-500">
+        Failed to load events
+      </p>
+    )
+  if (!events)
+    return (
+      <p className="text-center text-xl mt-20 text-black-500">...loading</p>
+    )
+  if (events.length <= 0)
+    return <p className="text-center text-xl mt-20 text-black-500">No events</p>
 
   return (
-    <div className="md:flex">
-      <Table>
-        <TableHeader className="w-full bg-gray-200 justify-center">
+    <div className="md:flex w-full overflow-x-auto">
+      <Table className="w-full border rounded-md">
+        <TableHeader className="bg-gray-200">
           <TableRow>
-            <TableHead className="text-start px-2">Date</TableHead>
-            <TableHead className="text-center px-2">Location</TableHead>
-            <TableHead className="text-end px-2">Theme</TableHead>
+            <TableHead className="w-[130px] text-start px-2">Date</TableHead>
+            <TableHead className="w-[100px] text-start px-2">Time</TableHead>
+            <TableHead className="w-[180px] text-center px-2">
+              Location
+            </TableHead>
+            <TableHead className="w-[250px] text-start px-2">Theme</TableHead>
+            <TableHead className="text-start px-2">Description</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          {events.map((event:any) => (
+          {events.map((event: any) => (
             <TableRow key={event?.eventDate} className="h-14">
               <TableCell className="text-start px-2">
                 {event?.eventDate}
               </TableCell>
+              <TableCell className="text-start px-2">
+                {event?.eventTime}
+              </TableCell>
               <TableCell className="text-center px-2">
                 {event?.eventLocation}
               </TableCell>
-              <TableCell className="text-end px-2">
+              <TableCell className="text-start px-2">
                 {event?.eventTheme}
+              </TableCell>
+              <TableCell className="text-start px-2 whitespace-normal break-words max-w-[300px]">
+                {event?.eventDescription}
               </TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
     </div>
-  );
+  )
 }
